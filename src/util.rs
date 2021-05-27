92628792f877263fdc8269600549d0dae84dae73
Jackz <me@jackz.me>
2021-05-26 23:34:13 -0500

    Add util method

---
+++ src/util.rs
@@ -0,0 +1,9 @@
+use indicatif::ProgressBar;
+use std::{borrow::Cow};
+
+pub fn setup_spinner(msg: impl Into<Cow<'static, str>>) -> ProgressBar {
+    let spinner: ProgressBar = ProgressBar::new_spinner()
+        .with_message(msg);
+    spinner.enable_steady_tick(1000u64);
+    spinner
+}
