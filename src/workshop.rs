b218e8e77d159350a10871c2c1cdfd9797313e56
Jackz <me@jackz.me>
2021-05-27 20:00:47 -0500

    add Workshop::search_full, move Workshop::search -> Workshop::search_ids

--- src/workshop.rs
+++ src/workshop.rs
@@ -37,14 +37,22 @@ pub struct WorkshopItemTag {
 
 #[doc(hidden)]
 #[derive(Serialize, Deserialize)]
-struct WSResponse {
-    response: WSResponseBody
+struct WSResponse<T> {
+    response: WSResponseBody<T>
 }
 
 #[doc(hidden)]
 #[derive(Serialize, Deserialize)]
-struct WSResponseBody {
-    publishedfiledetails: Vec<WorkshopItem>
+struct WSResponseBody<T> {
+    publishedfiledetails: Vec<T>
+}
+
+#[doc(hidden)]
+#[derive(Serialize, Deserialize)]
+struct WSSearchBody {
+    result: u8,
+    publishedfileid: String,
+    language: u8
 }
 
 #[derive(Serialize, Deserialize)]
@@ -123,13 +131,13 @@ impl Workshop {
             let name = format!("publishedfileids[{i}]", i=i);
             params.insert(name, vpk.to_string());
         }
-        let details: WSResponse = self.client
+        let details: WSResponse<WorkshopItem> = self.client
             .post("https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/")
             .header("User-Agent", format!("L4D2-Workshop-Downloader/v{}", env!("CARGO_PKG_VERSION")))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .form(&params)
             .send()?
-            .json::<WSResponse>()?;
+            .json::<WSResponse<WorkshopItem>>()?;
     
         let mut details_final: Vec<WorkshopItem> = Vec::new();
     
@@ -140,24 +148,53 @@ impl Workshop {
         Ok(details_final)
     }
 
+    //TODO: Extract into builder
     ///Search for workshop items
-    pub fn search(&self, appid: u64, query: &str) -> Result<Vec<WorkshopItem>, reqwest::Error> {
+    pub fn search_ids(&self, appid: u64, query: &str) -> Result<Vec<String>, reqwest::Error> {
         if let None = &self.apikey {
             panic!("No Steam Web API key was specified");
         }
 
-        /*let details = &self.client.get("https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?")
+        let details = &self.client.get("https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?")
             .header("User-Agent", format!("L4D2-Workshop-Downloader/v{}", env!("CARGO_PKG_VERSION")))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .query(&[
                 ("page", "1"),
+                ("numperpage", "20"),
                 ("search_text", query),
                 ("appid", &appid.to_string()),
-                ("key", &self.apikey.unwrap()),
+                ("key", &self.apikey.as_ref().unwrap()),
             ])
             .send()?
-            .json::<WSResponse>()?;
-        */
-        Ok(vec![])
+            .json::<WSResponse<WSSearchBody>>()?;
+
+        let mut fileids: Vec<String> = Vec::new();
+
+        for res in &details.response.publishedfiledetails {
+            fileids.push(res.publishedfileid.to_string());
+        }
+        Ok(fileids)
+    }
+
+    pub fn search_full(&self, appid: u64, query: &str) -> Result<Vec<WorkshopItem>, reqwest::Error> {
+        if let None = &self.apikey {
+            panic!("No Steam Web API key was specified");
+        }
+
+        let details = &self.client.get("https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/?")
+            .header("User-Agent", format!("L4D2-Workshop-Downloader/v{}", env!("CARGO_PKG_VERSION")))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .query(&[
+                ("page", "1"),
+                ("numperpage", "20"),
+                ("search_text", query),
+                ("appid", &appid.to_string()),
+                ("return_metadata", "1"),
+                ("key", &self.apikey.as_ref().unwrap()),
+            ])
+            .send()?
+            .json::<WSResponse<WorkshopItem>>()?;
+
+        Ok(details.response.publishedfiledetails.clone())
     }
 }
