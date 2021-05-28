fc5192a93061154cb24e766db57fc68ab3be3a1d
Jackz <me@jackz.me>
2021-05-27 20:00:54 -0500

    Implement basic search

--- src/menu_search.rs
+++ src/menu_search.rs
@@ -1,9 +1,9 @@
-use crate::workshop::Workshop;
+use crate::workshop::{Workshop, WorkshopItem};
 use crate::util;
 use crate::meta;
 
 use dialoguer::{theme::ColorfulTheme, Input};
-
+use console::style;
 
 pub fn handler(_config: &meta::Config , workshop: &Workshop) -> Result<(), Box<dyn std::error::Error>> {
     let input : String = Input::new()
@@ -11,7 +11,19 @@ pub fn handler(_config: &meta::Config , workshop: &Workshop) -> Result<(), Box<d
         .interact_text()?;
 
     let spinner = util::setup_spinner("Fetching search results...");
-    let results = workshop.search(550, &input);
+    let results: Vec<WorkshopItem> = match workshop.search_full(550, &input) {
+        Ok(results) => results,
+        Err(err) => {
+            println!("{} {}", 
+                style("Error").bold().red(),
+                style(err).red()
+            );
+            return Ok(())
+        }
+    };
+    for (i, item) in results.iter().enumerate() {
+        println!("{}. {}", i, item.title);
+    }
     spinner.finish_and_clear();
 
     Ok(())
