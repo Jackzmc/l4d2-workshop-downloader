67c0799736c4cec793e12195d7f0a639d68e631c
Jackz <me@jackz.me>
2021-05-31 00:33:49 -0500

    Cleanup import

--- src/menu_import.rs
+++ src/menu_import.rs
@@ -11,10 +11,18 @@ const MAX_ITEMS_PER_PAGE: usize = 20;
 pub fn handler(config: &mut meta::Config, workshop: &Workshop) -> Result<Option<util::MenuResult>, Box<dyn std::error::Error>> {
     //Fetch the current vpks in the workshop directory
     let spinner = util::setup_spinner("Fetching VPKS...");
-    let fileids = Workshop::get_vpks_in_folder(&_config.gamedir.join("workshop"))?;
+    let fileids = match Workshop::get_vpks_in_folder(&config.gamedir.join("workshop")) {
+        Ok(results) => results,
+        Err(err) => {
+            eprintln!("Failed to find VPKs in folder \"{}\": \n{}\n", &config.get_game_path_str().unwrap(), err);
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            return Ok(None)
+        }
+    };
     spinner.finish_with_message("Fetched VPKs");
 
     if fileids.is_empty() {
+        println!("There are no items to be imported.");
         return Ok(None)
     }
 
@@ -31,6 +39,7 @@ pub fn handler(config: &mut meta::Config, workshop: &Workshop) -> Result<Option<
 
     //Setup the list of selected vpks to import, pagination
     let mut selected_vpks: Vec<DownloadEntry> = Vec::with_capacity(fileids.len());
+    let mut page_items: Vec<String> = Vec::with_capacity(MAX_ITEMS_PER_PAGE);
     let size = fileids.len();
     page_items.reserve(MAX_ITEMS_PER_PAGE);
     selected_vpks.reserve(size);
@@ -68,19 +77,24 @@ pub fn handler(config: &mut meta::Config, workshop: &Workshop) -> Result<Option<
 
     let item_count = selected_vpks.len();
     if Confirm::with_theme(&ColorfulTheme::default())
-        .with_prompt(format!("Importing {} / {} workshop items, continue?", item_count, size))
+        .with_prompt(format!("Are you sure you want to import {} / {} workshop items?", item_count, size))
         .default(true)
         .interact()
         .unwrap()
     {
         //Finally, write the meta info
-        let dest_folder = _config.gamedir.clone();
-        fs::write(dest_folder.join("downloads.json"), serde_json::to_string(&selected_vpks)?)?;
+        let dest_folder = config.gamedir.clone();
+        let src_folder = config.gamedir.join("workshop");
         //Loop each selected item and move it down a directory (addons/workshop -> addons/)
-        for item in selected_vpks {
-            fs::rename(dest_folder.join(format!("workshop/{}.vpk", item.publishedfileid)), dest_folder.join(format!("{}.vpk", item.publishedfileid)))?;
+        for download in selected_vpks {
+            let filename = format!("{}.vpk", &download.publishedfileid);
+            fs::rename(src_folder.join(&filename), dest_folder.join(&filename))?;
+            config.add_download(download);
         }
-        println!("Succesfully imported {} files", item_count);
+        match config.save() {
+            Ok(()) => println!("Succesfully imported {} files", item_count),
+            Err(err) => eprintln!("Failed to import files: {}", err)
+        };
     } else {
         println!("Import was cancelled.");
     }
