5ac963d58af3bfd153a3e15e02af746a0325c0ef
Jackz <me@jackz.me>
2021-05-31 00:57:41 -0500

    menu_update: non-working indicatif

--- src/menu_update.rs
+++ src/menu_update.rs
@@ -4,12 +4,24 @@ use std::{fs};
 use futures::{stream, StreamExt};
 use indicatif::{HumanDuration};
 use dialoguer::{theme::ColorfulTheme, Confirm};
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, MultiProgress, ProgressStyle};
 use std::clone::Clone;
 use steamwebapi::{Workshop, WorkshopItem};
 use tokio::runtime::Runtime;
+use std::io::Write;
+use futures::{Future, future::BoxFuture, FutureExt};
+
+struct Download<'a> {
+    file: std::fs::File,
+    url: &'a str,
+    resp: Option<bytes::Bytes>,
+    item: &'a WorkshopItem,
+    pb: ProgressBar
+}
 
 const CONCURRENT_REQUESTS: usize = 4;
+const CHUNK_SIZE: u64 = 512000;
+
 
 pub fn handler(config: &meta::Config, workshop: &Workshop) -> Result<Option<util::MenuResult>, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
@@ -30,7 +42,7 @@ pub fn handler(config: &meta::Config, workshop: &Workshop) -> Result<Option<util
     //Using above list, get the latest workshop info (key is time_updated)
     let spinner = util::setup_spinner("Fetching Latest File Info...");
     let details = workshop.get_file_details(&fileids).expect("Failed to get VPK details");
-    spinner.finish_with_message("Fetched");
+    spinner.finish_and_clear();
 
     let mut outdated: Vec<WorkshopItem> = Vec::with_capacity(fileids.len());
 
@@ -39,8 +51,12 @@ pub fn handler(config: &meta::Config, workshop: &Workshop) -> Result<Option<util
         //Check if any entry in meta is outdated
         if details[i].time_updated >= entry.time_updated {
             let duration = std::time::Duration::from_secs(details[i].time_updated as u64 - entry.time_updated as u64);
-            let hd = HumanDuration(duration);
-            println!("Item \"{title}\" is out of date. {hd}", title=entry.title, hd=hd);
+            if duration.as_secs() < 1800 {
+                println!("Item \"{title}\" is out of date. Last updated recently", title=entry.title);
+            }else {
+                let hd = HumanDuration(duration);
+                println!("Item \"{title}\" is out of date. Last update was {hd} ago.", title=entry.title, hd=hd);
+            }
             outdated.push(details[i].clone());
         }
     }
@@ -58,56 +74,90 @@ pub fn handler(config: &meta::Config, workshop: &Workshop) -> Result<Option<util
     {
         let directory = config.gamedir.clone();
 
-        struct Download {
-            file: std::fs::File,
-            url: String
-        }
         let mut downloads: Vec<Download> = Vec::with_capacity(outdated.len());
+        std::io::stderr().flush().ok();
+        std::io::stdout().flush().ok();
+
+        let mpb = MultiProgress::new();
+        let style = ProgressStyle::default_bar()
+            .template("{spinner:.green} {msg} - [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+            .progress_chars("#>-");
+
+        for item in outdated.iter().take(9) {
+            
+            let dest = {
+                let fname = directory.join(format!("{}.vpk", item.publishedfileid));
+                fs::File::create(fname).expect("Could not create file")
+            };
+            let pb = ProgressBar::new(item.file_size as u64)
+                .with_message(format!("{} ({}.vpk)", &item.title, &item.publishedfileid))
+                .with_style(style.clone());
+            let download = Download {
+                file: dest,
+                url: &item.file_url,
+                resp: None,
+                item: &item,
+                pb: mpb.add(pb)
+            };
+            downloads.push(download);
+        }
+        mpb.set_move_cursor(true);
+        mpb.set_draw_target(indicatif::ProgressDrawTarget::stdout_with_hz(2000));
+
         let rt = Runtime::new()?;
         rt.block_on(async {
-            //Section is WIP, need to have || threads to download and hopefully update progress bar w/o blocking
-            let spinner = util::setup_spinner(format!("Updating {} items", outdated.len()));
-            for item in outdated.iter() {
-                let directory = directory.clone();
-                let pb: ProgressBar = ProgressBar::new(item.file_size as u64)
-                    .with_message(format!("{} as {}.vpk", item.title, item.publishedfileid))
-                    .with_style(ProgressStyle::default_bar()
-                        .template("{spinner:.green} {msg} - [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-                        .progress_chars("#>-")
-                    );
-                let dest = {
-                    let fname = directory.join(format!("{}.vpk", item.publishedfileid));
-                    fs::File::create(fname).expect("Could not create file")
-                };
-                let download = Download {
-                    file: dest,
-                    url: item.file_url.clone()
-                };
-                downloads.push(download);
-            }
 
+            //println!("> Starting download");
             stream::iter(downloads)
-            .map(|download| {
+            .map(|mut download: Download| {
                 let client = &client;
                 async move {
-                    let resp = client
-                        .get(download.url)
+                    //println!("Downloading {}", &download.item);
+                    download.pb.println("item.");
+                    match client
+                        .get("http://mc.jackz.me/100MB.zip")
                         .header("User-Agent", "L4D2-Workshop-Downloader")
                         .send()
-                        .await?;
-                    resp.bytes().await
+                        .await
+                        
+                    {
+                        Ok(response) => {
+                            match response.bytes().await {
+                                Ok(bytes) => {
+                                    for chunk in bytes.chunks_exact(CHUNK_SIZE as usize) {
+                                        //download.file.write(&chunk).expect("write err");
+                                        download.pb.inc(CHUNK_SIZE);
+                                        download.pb.println("chunk dl");
+                                        download.pb.tick();
+                                    }
+                                    download.resp = Some(bytes);
+                                },
+                                Err(err) => eprintln!("Item {} failed: {}", &download.item, err)
+                            }
+                            //download.pb.finish()
+                        },
+                        Err(err) => {
+                            //download.pb.finish_with_message(format!("Item {} failed: {}", &download.item, err));
+                        }
+                    }
+                    download.pb.finish();
+                    download
                 }
             })
             .buffer_unordered(CONCURRENT_REQUESTS)
-            .for_each(|b| async {
-                match b {
-                    Ok(b) => println!("Got {} bytes", b.len()),
-                    Err(e) => eprintln!("Got an error: {}", e),
+            .for_each(|download| async {
+                match download.resp {
+                    Some(resp) => {
+                        println!("Downloaded {} bytes for {}", resp.len(), &download.item);
+                    },
+                    None => println!("item empty {}", &download.item)
                 }
+                mpb.remove(&download.pb);
             })
             .await;
-            spinner.finish();
-        })
+            tokio::task::spawn_blocking(move || mpb.join().expect("Progress bar failure"));
+        });
+        println!("COMPLETE!");
     } else {
         println!("Update was cancelled. Returning to menu.");
     }
