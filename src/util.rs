6abdb9cc4498b17b4941772600e33afc8637df06
Jackz <me@jackz.me>
2021-06-02 11:05:27 -0500

    add regex struct to util

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,8 @@
 use indicatif::ProgressBar;
 use std::{borrow::Cow};
-
+use regex::Regex;
+use lazy_static::lazy_static;
+    
 pub struct MenuResult {
 }
 
@@ -9,4 +11,30 @@ pub fn setup_spinner(msg: impl Into<Cow<'static, str>>) -> ProgressBar {
         .with_message(msg);
     spinner.enable_steady_tick(1000u64);
     spinner
+}
+
+pub struct Regexes {}
+impl Regexes {
+    pub fn get_filename_addonid(filename: &str) -> Option<String>  {
+        lazy_static! {
+            static ref REGEX: Regex = Regex::new(r"([0-9]{7,})").unwrap();
+        }
+        if let Some(mat) = REGEX.find(&filename) {
+            return Some(filename[mat.start()..mat.end()].to_string());
+        } else {
+            None
+        }
+    }
+    pub fn get_id_from_workshop_url(url: &str) -> Option<String> {
+        lazy_static! {
+            static ref REGEX: Regex = Regex::new(r"https?://steamcommunity.com/(workshop|sharedfiles)/filedetails/\?id=([0-9]+)").unwrap();
+        }
+        //https://steamcommunity.com/sharedfiles/filedetails/?id=1558774205
+        if let Some(caps) = REGEX.captures(url) {
+            if caps.len() == 3 {
+                return Some(caps[2].to_string())
+            } 
+        }
+        None
+    }
 }
