a8c1396acc5c0246269567052f0f8d25ce0f42d0
Jackz <me@jackz.me>
2021-06-03 12:12:01 -0500

    Open file with append, use writeln!

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,8 @@
 use std::{fs,path};
 use chrono::prelude::*;
 use console::style;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 pub struct Logger {
     file: fs::File
@@ -23,24 +25,28 @@ impl LogLevel {
 impl Logger {
     pub fn new(filepath: path::PathBuf) -> Logger {
         Logger {
-            file: fs::File::create(filepath).unwrap()
+            file: OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(filepath)
+                .unwrap()
         }
     }
 
-    pub fn log(&self, level: LogLevel, msg: &'static str) {
+    pub fn log(&self, level: LogLevel, msg: &str) {
         let now: DateTime<Local> = Local::now();
         let timestamp = now.format("%Y/%m/%d");
-        write!(&self.file, "[{}] [{}] {}", 
+        writeln!(&self.file, "[{}] [{}] {}", 
             timestamp,
             level.name(),
             msg
         );
     }
 
-    pub fn logp(&self, level: LogLevel, prefix: &'static str, msg: &'static str) {
+    pub fn logp(&self, level: LogLevel, prefix: &'static str, msg: &str) {
         let now: DateTime<Local> = Local::now();
         let timestamp = now.format("%Y/%m/%d");
-        write!(&self.file, "[{}] [{}/{}] {}", 
+        writeln!(&self.file, "[{}] [{}/{}] {}", 
             timestamp,
             prefix,
             level.name(),
@@ -48,26 +54,26 @@ impl Logger {
         );
     }
 
-    pub fn info(&self, prefix: &'static str, msg: &'static str) {
+    pub fn info(&self, prefix: &'static str, msg: &str) {
         self.logp(LogLevel::INFO, prefix, &msg);
     }
 
-    pub fn warn(&self, prefix: &'static str, msg: &'static str) {
+    pub fn warn(&self, prefix: &'static str, msg: &str) {
         self.logp(LogLevel::WARN, prefix, &msg);
         println!("{}", style(msg).yellow());
     }
 
-    pub fn success(&self, prefix: &'static str, msg: &'static str) {
+    pub fn success(&self, prefix: &'static str, msg: &str) {
         self.logp(LogLevel::WARN, prefix, &msg);
         println!("{}", style(msg).green());
     }
 
-    pub fn error(&self, prefix: &'static str, msg: &'static str) {
+    pub fn error(&self, prefix: &'static str, msg: &str) {
         self.logp(LogLevel::ERROR, prefix, &msg);
         eprintln!("{} {}", style("Error: ").red().bold(), style(msg).red());
     }
 
-    pub fn debug(&self, prefix: &'static str, msg: &'static str) {
+    pub fn debug(&self, prefix: &'static str, msg: &str) {
         self.logp(LogLevel::DEBUG, prefix, &msg);
         println!("{}", style(msg).magenta());
     }
