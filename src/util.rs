ea761acb52251447bde72af3acbdea5d696ff97e
Jackz <me@jackz.me>
2021-06-04 12:01:11 -0500

    add util::download_addons()

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,11 @@
-use indicatif::ProgressBar;
-use std::{borrow::Cow};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::{borrow::Cow, fs, io::Write};
 use regex::Regex;
 use lazy_static::lazy_static;
+use tokio::runtime::Runtime;
+use futures::{stream, StreamExt};
 
-use crate::meta::Config;
+use crate::meta::{Config, DownloadEntry};
 use crate::logger::Logger;
 use steamwebapi::Workshop;
 
@@ -60,4 +62,111 @@ pub fn format_bytes(bytes: u64) -> String {
     } else {
         format!("{} B", bytes)
     }
+}
+
+
+const CONCURRENT_REQUESTS: usize = 4;
+
+struct Download {
+    file: std::fs::File,
+    success: bool,
+    item: steamwebapi::WorkshopItem,
+}
+
+pub fn download_addons(menu: &mut MenuParams, items: &[steamwebapi::WorkshopItem]) -> Result<(), Box<dyn std::error::Error>> {
+    let progress = ProgressBar::new(items.len() as u64)
+    .with_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:60.cyan/blue}] {pos} / {len} items updated ({percent}%)")
+        .progress_chars("#>-")
+        .tick_strings(&[
+            "↓    ",
+            "↓ .  ",
+            "↓ .. ",
+            "↓ ...",
+            ""
+        ])
+        //"―\\|/―\\|/―"
+    );
+
+    let mut downloads: Vec<Download> = Vec::with_capacity(items.len());
+    for item in items {
+        
+        let dest = {
+            let fname = menu.config.gamedir.join(format!("{}.vpk", item.publishedfileid));
+            fs::File::create(fname).expect("Could not create file")
+        };
+        let download = Download {
+            file: dest,
+            item: item.clone(),
+            success: false
+        };
+        downloads.push(download);
+    }
+    progress.tick();
+    progress.enable_steady_tick(500);
+
+    let client = reqwest::Client::new();
+
+    let rt = Runtime::new()?;
+    rt.block_on(async {
+        stream::iter(downloads)
+        .map(|mut download: Download| {
+            let client = &client;
+            let pb = &progress;
+            async move {
+                pb.set_message(format!("{}", &download.item.title));
+                match client
+                    .get(&download.item.file_url)
+                    .header("User-Agent", "L4D2-Workshop-Downloader")
+                    .send()
+                    .await
+                    
+                {
+                    Ok(response) => {
+                        let mut stream = response.bytes_stream();
+                        while let Some(result) = stream.next().await {
+                            match result {
+                                Ok(chunk) => {
+                                    if let Err(err) = download.file.write(&chunk) {
+                                        println!("[{}] Write Error: {}", &download.item.publishedfileid, err);
+                                        break;
+                                    }
+                                
+                                },
+                                Err(err) => {
+                                    println!("{}\n{}", 
+                                        console::style(format!("Download for {} failed:\n", &download.item.title)).red().bold(),
+                                        console::style(err).red()
+                                    );
+                                    break;
+                                }
+                            }
+                        }
+                        download.file.flush().ok();
+                        download.success = true;
+                    },
+                    Err(err) => {
+                        println!("Download failure for {}: {}", &download.item, err);
+                    }
+                }
+                download
+            }
+        })
+        .buffer_unordered(CONCURRENT_REQUESTS)
+        .for_each(|download| {
+            progress.inc(1);
+            let pb = &progress;
+
+            menu.config.update_download(DownloadEntry::from_item(&download.item));
+            menu.config.save().ok();
+
+            async move {
+                pb.println(format!("Updated {} as {}.vpk", &download.item.title, &download.item.publishedfileid));
+            }
+        })
+        .await;
+        progress.finish_and_clear();
+
+    });
+    Ok(())
 }
